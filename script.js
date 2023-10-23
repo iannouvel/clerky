bf56fa145fc6b2b597aebcb4a75c0e5ea2f791fa
iannouvel <62343153+iannouvel@users.noreply.github.com>
2023-10-23 17:33:24 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,73 +1,35 @@
-const situationDropdown = document.getElementById('situationDropdown');
-const parametersFrame = document.getElementById('parametersFrame');
-const adviceFrame = document.getElementById('adviceFrame');
-const decisionDropdown = document.getElementById('decisionDropdown');
-const agreedPlanFrame = document.getElementById('agreedPlanFrame');
-const copyToSummaryBtn = document.getElementById('copyToSummaryBtn');
-const summaryTextarea = document.getElementById('summaryTextarea');
-const copyToClipboardBtn = document.getElementById('copyToClipboardBtn');
-
-situationDropdown.addEventListener('change', function() {
-    switch (situationDropdown.value) {
-        case 'induction':
-            parametersFrame.style.display = 'block';
-            parametersFrame.contentDocument.body.innerHTML = `
-                <select id="parameterSelect">
-                    <option value="select" disabled selected>Select a Parameter</option>
-                    <option value="macrosomia">Macrosomia</option>
-                    <option value="advancedMaternalAge">Advanced Maternal Age</option>
-                </select>
-            `;
-            break;
+function loadPage() {
+    const dropdown = document.getElementById('situationDropdown');
+    const iframe = document.getElementById('adviceFrame');
+    if (dropdown.value !== "default") {
+        iframe.src = dropdown.value;
+        iframe.classList.remove('hidden');
+        document.querySelectorAll('.hidden').forEach(el => el.classList.remove('hidden'));
+    } else {
+        iframe.classList.add('hidden');
+        document.getElementById('pasteToSummaryBtn').classList.add('hidden');
+        document.getElementById('summaryTextbox').classList.add('hidden');
+        document.getElementById('copyToClipboardBtn').classList.add('hidden');
+        document.querySelectorAll('.red-line').forEach(el => el.classList.add('hidden'));
     }
-});
+}
 
-parametersFrame.onload = function() {
-    const parameterSelect = parametersFrame.contentDocument.getElementById('parameterSelect');
-    parameterSelect.addEventListener('change', function() {
-        switch (parameterSelect.value) {
-            case 'macrosomia':
-                adviceFrame.style.display = 'block';
-                adviceFrame.contentDocument.body.innerHTML = "Macrosomia is associated with X, can choose expectant, sweeps, sweeps and induction, induction, CS";
-                
-                decisionDropdown.style.display = 'block';
-                decisionDropdown.innerHTML = `
-                    <option value="select" disabled selected>Select a Decision</option>
-                    <option value="expectant">Expectant</option>
-                    <option value="sweeps">Sweeps</option>
-                    <option value="sweepsAndInduction">Sweeps and Induction</option>
-                    <option value="induction">Induction</option>
-                    <option value="CS">CS</option>
-                `;
-                break;
-            // ... Add cases for other parameters ...
-        }
-    });
-};
-
-decisionDropdown.addEventListener('change', function() {
-    agreedPlanFrame.style.display = 'block';
-    switch (decisionDropdown.value) {
-        case 'expectant':
-            agreedPlanFrame.contentDocument.body.innerHTML = 'Decided for expectant management, can change decision and if so to inform CMW';
-            break;
-        case 'sweeps':
-            agreedPlanFrame.contentDocument.body.innerHTML = 'Sweeps';
-            break;
-        // ... Add cases for other decisions ...
+async function pasteFromClipboard() {
+    try {
+        const clipboardText = await navigator.clipboard.readText();
+        const summaryTextbox = document.getElementById('summaryTextbox');
+        summaryTextbox.value += clipboardText;
+    } catch (err) {
+        console.error('Failed to read clipboard:', err);
     }
-});
+}
 
-copyToSummaryBtn.addEventListener('click', function() {
-    summaryTextarea.style.display = 'block';
-    copyToClipboardBtn.style.display = 'block';
-
-    summaryTextarea.value += adviceFrame.contentDocument.body.innerText + "\n";
-    summaryTextarea.value += decisionDropdown.options[decisionDropdown.selectedIndex].text + "\n";
-    summaryTextarea.value += agreedPlanFrame.contentDocument.body.innerText + "\n";
-});
-
-copyToClipboardBtn.addEventListener('click', function() {
-    summaryTextarea.select();
-    document.execCommand('copy');
-});
+async function copyToClipboard() {
+    try {
+        const summaryTextbox = document.getElementById('summaryTextbox');
+        await navigator.clipboard.writeText(summaryTextbox.value);
+        alert('Summary copied to clipboard!');
+    } catch (err) {
+        console.error('Failed to copy text:', err);
+    }
+}
