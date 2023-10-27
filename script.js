a88aead37f81a0d351c29abbf0c9c96c7add8561
iannouvel <62343153+iannouvel@users.noreply.github.com>
2023-10-27 23:35:40 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,35 +1,30 @@
-function loadPage() {
-    const dropdown = document.getElementById('situationDropdown');
-    const iframe = document.getElementById('adviceFrame');
-    if (dropdown.value !== "default") {
-        iframe.src = dropdown.value;
-        iframe.classList.remove('hidden');
-        document.querySelectorAll('.hidden').forEach(el => el.classList.remove('hidden'));
+function updateIframe() {
+    const situationDropdown = document.getElementById('situationDropdown');
+    const adviceFrame = document.getElementById('adviceFrame');
+    const hiddenElements = document.querySelectorAll('.hidden');
+
+    if (situationDropdown.value) {
+        adviceFrame.src = situationDropdown.value;
+        hiddenElements.forEach(element => element.classList.remove('hidden'));
     } else {
-        iframe.classList.add('hidden');
-        document.getElementById('pasteToSummaryBtn').classList.add('hidden');
-        document.getElementById('summaryTextbox').classList.add('hidden');
-        document.getElementById('copyToClipboardBtn').classList.add('hidden');
-        document.querySelectorAll('.red-line').forEach(el => el.classList.add('hidden'));
+        adviceFrame.src = '';
+        hiddenElements.forEach(element => element.classList.add('hidden'));
     }
 }
 
 async function pasteFromClipboard() {
     try {
-        const clipboardText = await navigator.clipboard.readText();
+        const text = await navigator.clipboard.readText();
         const summaryTextbox = document.getElementById('summaryTextbox');
-        summaryTextbox.value += clipboardText;
+        summaryTextbox.value += text;
     } catch (err) {
         console.error('Failed to read clipboard:', err);
     }
 }
 
-async function copyToClipboard() {
-    try {
-        const summaryTextbox = document.getElementById('summaryTextbox');
-        await navigator.clipboard.writeText(summaryTextbox.value);
-        //alert('Summary copied to clipboard!');
-    } catch (err) {
-        console.error('Failed to copy text:', err);
-    }
+function copyToClipboard() {
+    const summaryText = document.getElementById('summaryTextbox').value;
+    navigator.clipboard.writeText(summaryText)
+        .then(() => console.log('Summary copied to clipboard.'))
+        .catch(err => console.error('Failed to copy:', err));
 }
