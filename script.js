3f22d1050ab1661f5017b0ef1e29078fe69983ad
iannouvel <62343153+iannouvel@users.noreply.github.com>
2023-11-12 13:28:17 +0000

    Update script.js

--- script.js
+++ script.js
@@ -15,11 +15,18 @@ function changeIFrameSource() {
 async function pasteToSummary() {
     try {
         const text = await navigator.clipboard.readText();
-        document.getElementById("summary").value += text;
+        const summaryElement = document.getElementById("summary");
+        // Add a newline character before the text if the textarea is not empty
+        if (summaryElement.value) {
+            summaryElement.value += '\n' + text;
+        } else {
+            summaryElement.value = text;
+        }
     } catch (err) {
         console.error('Failed to read clipboard contents: ', err);
     }
 }
+
 
 function copyToClipboard() {
     const summaryText = document.getElementById("summary").value;
