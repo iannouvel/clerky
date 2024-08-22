6a6b8a3268258d3c4791fa9e83b90fcd2de31752
Ian Nouvel <62343153+iannouvel@users.noreply.github.com>
2024-08-22 16:40:55 +0100

    Update extract_keywords.py

--- scripts/extract_keywords.py
+++ scripts/extract_keywords.py
@@ -44,7 +44,7 @@ def condense_chunk(chunk):
     prompt = (
         "With the attached text from a clinical guideline, "
         "please return a condensed version of the text which removes clinically insignificant text, "
-        "please remove all the references, if there are any, at the end of the text as they do not need to be in the condensed output, "
+        "please remove all the scientific references, if there are any, at the end of the text as they do not need to be in the condensed output, "
         "please do not change the clinically significant text at all.\n\n"
         f"{chunk}"
     )
