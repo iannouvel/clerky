fdad36b01ccbfb0420611f8931ed5cf4c1216f04
Ian Nouvel <62343153+iannouvel@users.noreply.github.com>
2025-01-20 15:29:35 +0000

    algos

--- firebase-init.js
+++ firebase-init.js
@@ -1,6 +1,5 @@
 // Import Firebase modules
 import { initializeApp } from 'https://www.gstatic.com/firebasejs/10.14.1/firebase-app.js';
-import { getAnalytics } from 'https://www.gstatic.com/firebasejs/10.14.1/firebase-analytics.js';
 import { getFirestore } from 'https://www.gstatic.com/firebasejs/10.14.1/firebase-firestore.js';
 import { getAuth } from 'https://www.gstatic.com/firebasejs/10.14.1/firebase-auth.js';
 
@@ -13,9 +12,11 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
 const db = getFirestore(app);
 const auth = getAuth(app);
 
+// Make auth available globally
+window.firebase = { auth };
+
 // Export initialized instances
-export { app, analytics, db, auth }; 
+export { app, db, auth }; 
