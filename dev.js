1090510c102d706cc92e41aaff7544c37ce7eadc
Ian Nouvel <62343153+iannouvel@users.noreply.github.com>
2025-03-07 13:07:36 +0000

    dev

--- dev.js
+++ dev.js
@@ -1,6 +1,6 @@
 // Import Firebase modules
-import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.x/firebase-app.js';
-import { getAuth, onAuthStateChanged } from 'https://www.gstatic.com/firebasejs/9.x/firebase-auth.js';
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js';
+import { getAuth, onAuthStateChanged } from 'https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js';
 
 // Global variables
 const SERVER_URL = 'https://clerky-uzni.onrender.com';
